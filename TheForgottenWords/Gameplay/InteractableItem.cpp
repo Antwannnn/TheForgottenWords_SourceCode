ef18e140ee131442a34e310f1a9fbda4a5945698
Atsura <94127380+Antwannnn@users.noreply.github.com>
2022-08-22 13:37:18 +0200

    deleted post process volume class

--- TheForgottenWords/Gameplay/InteractableItem.cpp
+++ TheForgottenWords/Gameplay/InteractableItem.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
-#include "InteractableItem.h"
 #include "Components/StaticMeshComponent.h"
+#include "InteractableItem.h"
+
 
 // Sets default values
 AInteractableItem::AInteractableItem()
